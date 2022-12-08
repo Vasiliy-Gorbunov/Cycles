ebe94104ec3a1fec2a2ff0a20877ab8ab8d199c4
Vasiliy-Gorbunov <79896130110@yandex.ru>
2022-12-08 20:14:21 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,16 @@ public class Main {
     }
 
     public static void task1() {
-        System.out.println("Задача 1\n");
-        for (int i = 0; i <= 10; i++) {
-            System.out.println("Итерация цикла: " + i);
+        System.out.println("Задача 1:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
         }
     }
 
     public static void task2() {
-
+        System.out.println("\nЗадача 2:");
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
     }
 }
