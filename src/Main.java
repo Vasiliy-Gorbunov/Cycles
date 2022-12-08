039ed1844200ac1a65cf0cfd270f1921dc669f87
Vasiliy-Gorbunov <79896130110@yandex.ru>
2022-12-08 20:10:28 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1\n");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Итерация цикла: " + i);
+        }
+    }
+
+    public static void task2() {
+
+    }
+}
